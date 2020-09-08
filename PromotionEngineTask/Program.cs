bc4b99d4f73c2b53a7bf04977f0617429b01db34
meghaingle <inglemegha@gmail.com>
2020-09-08 15:01:50 +0530

    Modified main method and added dictionary for promo and orders

--- PromotionEngineTask/Program.cs
+++ PromotionEngineTask/Program.cs
@@ -10,7 +10,33 @@ namespace PromotionEngineTask
     {
         static void Main(string[] args)
         {
-            
+            //create list of promotions
+            //we need to add information about Product's count
+            Dictionary<String, int> d1 = new Dictionary<String, int>();
+            d1.Add("A", 3);
+            Dictionary<String, int> d2 = new Dictionary<String, int>();
+            d2.Add("B", 2);
+            Dictionary<String, int> d3 = new Dictionary<String, int>();
+            d3.Add("C", 1);
+            d3.Add("D", 1);
+
+            List<Promotion> promotions = new List<Promotion>()
+             {
+           
+                  new Promotion(1, d1, 130),
+       
+                  new Promotion(2, d2, 45),
+       
+                   new Promotion(3, d3, 30)
+             };
+
+            List<Order> orders = new List<Order>();
+
+            Order order1 = new Order(1, new List<Product>() { new Product("A"), new Product("A"), new Product("B"), new Product("B"), new Product("C"), new Product("D") });
+            Order order2 = new Order(2, new List<Product>() { new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("A"), new Product("B") });
+            Order order3 = new Order(3, new List<Product>() { new Product("A"), new Product("A"), new Product("D"), new Product("B"), new Product("B") });
+            orders.AddRange(new Order[] { order1, order2, order3 });
+
             Console.ReadLine();
         }
 
