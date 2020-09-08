c519e8638cb2e1293e97170793dd36f2a7face5f
meghaingle <inglemegha@gmail.com>
2020-09-08 14:57:18 +0530

    Modified Product class

--- PromotionEngineTask/Program.cs
+++ PromotionEngineTask/Program.cs
@@ -41,7 +41,30 @@ namespace PromotionEngineTask
         {
             public string Id { get; set; }
             public decimal Price { get; set; }
+            public Product(string id)
+            {
+                this.Id = id;
+                switch (id)
+                {
+                    case "A":
+                        this.Price = 50m;
+
+                        break;
+                    case "B":
+                        this.Price = 30m;
+
+                        break;
+                    case "C":
+                        this.Price = 20m;
+
+                        break;
+                    case "D":
+                        this.Price = 15m;
+                        break;
+                }
+            }
         }
+
 
         //public interface IProductService
         //{
