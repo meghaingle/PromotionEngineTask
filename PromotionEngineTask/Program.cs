435ab12178876e7d117b1c1b56ca967b045dc230
meghaingle <inglemegha@gmail.com>
2020-09-08 14:51:40 +0530

    Added Promotion checker static class

--- PromotionEngineTask/Program.cs
+++ PromotionEngineTask/Program.cs
@@ -27,13 +27,13 @@ namespace PromotionEngineTask
                 string type = Console.ReadLine();
                 Product product = new Product();
                 product.Id = type;
-                
+
 
                 productService.GetPriceByType(product);
                 products.Add(product);
             }
-             int totalPrice = productService.GetTotalPrice(products);
-             Console.WriteLine(totalPrice);
+            int totalPrice = productService.GetTotalPrice(products);
+            Console.WriteLine(totalPrice);
             Console.ReadLine();
         }
 
@@ -140,6 +140,28 @@ namespace PromotionEngineTask
             }
         }
 
+        public static class PromotionChecker
+        {
+            //returns PromotionID and count of promotions
+            public static decimal GetTotalPrice(Order ord, Promotion prom)
+            {
+                decimal d = 0M;
+                //get count of promoted products in order
+                var copp = ord.Products
+                    .GroupBy(x => x.Id)
+                    .Where(grp => prom.ProductInfo.Any(y => grp.Key == y.Key && grp.Count() >= y.Value))
+                    .Select(grp => grp.Count())
+                    .Sum();
+                //get count of promoted products from promotion
+                int ppc = prom.ProductInfo.Sum(kvp => kvp.Value);
+                while (copp >= ppc)
+                {
+                    d += prom.PromoPrice;
+                    copp -= ppc;
+                }
+                return d;
+            }
 
+        }
     }
 }
