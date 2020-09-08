b9217528bb7dd2c85293f2e40839c1c8549f3e14
meghaingle <inglemegha@gmail.com>
2020-09-08 14:58:52 +0530

    Removed Commented Code

--- PromotionEngineTask/Program.cs
+++ PromotionEngineTask/Program.cs
@@ -10,30 +10,7 @@ namespace PromotionEngineTask
     {
         static void Main(string[] args)
         {
-            //List<Product> products = new List<Product>();
-
-            //Console.WriteLine("Please enter total number of order:");
-            //int orderNo = 0;
-
-            //while (!int.TryParse(Console.ReadLine(), out orderNo))
-            //{
-            //    Console.WriteLine("Please Enter a valid numerical value!");
-            //}
-
-            //ProductService productService = new ProductService();
-            //for (int i = 0; i < orderNo; i++)
-            //{
-            //    Console.WriteLine("Please enter the type of product:A,B,C or D");
-            //    string type = Console.ReadLine();
-            //    Product product = new Product();
-            //    product.Id = type;
-
-
-            //    productService.GetPriceByType(product);
-            //    products.Add(product);
-            //}
-            //int totalPrice = productService.GetTotalPrice(products);
-            //Console.WriteLine(totalPrice);
+            
             Console.ReadLine();
         }
 
@@ -64,79 +41,7 @@ namespace PromotionEngineTask
                 }
             }
         }
-
-
-        //public interface IProductService
-        //{
-        //    void GetPriceByType(Product product);
-        //    int GetTotalPrice(List<Product> products);
-        //}
-
-        //public class ProductService : IProductService
-        //{
-        //    public void GetPriceByType(Product product)
-        //    {
-        //        switch (product.Id)
-        //        {
-        //            case "A":
-        //                product.Price = 50m;
-
-        //                break;
-        //            case "B":
-        //                product.Price = 30m;
-
-        //                break;
-        //            case "C":
-        //                product.Price = 20m;
-
-        //                break;
-        //            case "D":
-        //                product.Price = 2015m;
-        //                break;
-        //        }
-        //    }
-
-        //    public int GetTotalPrice(List<Product> products)
-        //    {
-        //        int counterOfA = 0;
-        //        int priceOfA = 50;
-        //        int counterOfB = 0;
-        //        int priceOfB = 30;
-        //        int counterOfC = 0;
-        //        int priceOfC = 20;
-        //        int counterOfD = 0;
-        //        int priceOfD = 15;
-
-        //        foreach (Product pr in products)
-        //        {
-        //            switch (pr.Id)
-        //            {
-        //                case "A":
-        //                case "a":
-        //                    counterOfA += 1;
-        //                    break;
-        //                case "B":
-        //                case "b":
-        //                    counterOfB += 1;
-        //                    break;
-        //                case "C":
-        //                case "c":
-        //                    counterOfC += 1;
-        //                    break;
-        //                case "D":
-        //                case "d":
-        //                    counterOfD += 1;
-        //                    break;
-        //            }
-        //        }
-        //        int totalPriceOfA = (counterOfA / 3) * 130 + (counterOfA % 3 * priceOfA);
-        //        int totalPriceOfB = (counterOfB / 2) * 45 + (counterOfB % 2 * priceOfB);
-        //        int totalPriceOfC = (counterOfC * priceOfC);
-        //        int totalPriceOfD = (counterOfD * priceOfD);
-        //        return totalPriceOfA + totalPriceOfB + totalPriceOfC + totalPriceOfD;
-        //    }
-        //}
-
+        
         public class Promotion
         {
             public int PromotionID { get; set; }
